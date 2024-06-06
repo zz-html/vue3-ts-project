b81dc930310010efbacbe39acddf93c5a8859847
zhengzhang <zhengzhang@ruijie.com.cn>
2024-06-06 11:21:13 +0800

    @配置

--- rule-system/vite.config.ts
+++ rule-system/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -21,5 +22,11 @@ export default defineConfig({
 
     // 配置开发服务器的主机
     host: '0.0.0.0',
-  }  
+  },
+  
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
